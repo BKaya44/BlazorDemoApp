bc8c04daa7e5be499e6ba74940dbf8b9e3517056
BKaya44 <11460681+BKaya44@users.noreply.github.com>
2024-03-13 15:19:07 +0100

    Add weather page

---
+++ BlazorDemoApp/Models/Weather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+public class WeatherForecast
+{
+    public List<string> Time { get; set; }
+    public List<double> Temperature_2m { get; set; }
+}
+
+public class WeatherApiResponse
+{
+    public WeatherForecast Hourly { get; set; }
+}
